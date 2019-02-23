2b2c50d2a1cbda2715a25124c1a06e4a47a6907a
felipess <felipessouza@outlook.com>
2019-02-22 23:03:31 -0300

    fixed transition

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 500) {
+    // if ($("#mainNav").offset().top > 500) {
+    if ($("#hidden").offset().top > 1) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
